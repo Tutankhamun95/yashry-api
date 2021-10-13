4b4e525a013bf1d76af8e6ab0a03efede24c2caa
Tutankhamun95 <ali-mahdy@windowslive.com>
2021-10-14 06:49:36 +0800

    before testing

--- api/delete.php
+++ api/delete.php
@@ -4,29 +4,19 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
 include_once '../database/database.php';
 include_once '../models/employees.php';
 $database = new Database();
 $db = $database->getConnection();
 $item = new Employee($db);
+
 $item->id = isset($_GET['id']) ? $_GET['id'] : die();
-$item->getSingleEmployee();
-if($item->name != null){
 
-// create array
-$emp_arr = array(
-"id" => $item->id,
-"name" => $item->name,
-"email" => $item->email,
-"designation" => $item->designation,
-"created" => $item->created
-);
-
-http_response_code(200);
-echo json_encode($emp_arr);
-}
-else{
-http_response_code(404);
-echo json_encode("Employee not found.");
+if($item->deleteEmployee()){
+echo json_encode("Employee deleted.");
+} else{
+echo json_encode("Data could not be deleted");
 }
 ?>
--- api/update.php
+++ api/update.php
@@ -5,8 +5,8 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-include_once '../database.php';
-include_once '../employees.php';
+include_once '../database/database.php';
+include_once '../database/employees.php';
 
 $database = new Database();
 $db = $database->getConnection();
